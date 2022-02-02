ab682e2308ab66d0417781ec7ab0a886d291a0d4
Nico <nicolas.hardmeier@yahoo.es>
2022-02-03 00:05:13 +0100

    fix items

--- src/components/EmployeesListItem.tsx
+++ src/components/EmployeesListItem.tsx
@@ -2,7 +2,25 @@ import { useState } from 'react';
 
 const EmployeesListItem = ({ employee }: { employee: any }) => {
 
-    const [activeStep, setActiveStep] = useState<number>(1)
+    const [activeStep, setActiveStep] = useState<number>(0)
+
+    const steps = [
+        {
+            text: "added"
+        },
+        {
+            text: "in-check"
+        },
+        {
+            text: "approved"
+        },
+        {
+            text: "active"
+        },
+        {
+            text: "inactive"
+        }
+    ]
 
     return (
         <>
@@ -11,34 +29,17 @@ const EmployeesListItem = ({ employee }: { employee: any }) => {
                     <div className="employeesListItem__container">
                         <span>{employee.name}</span>
                         <div className="employeesListItemStatus__container">
-                            <div
-                                onClick={() => setActiveStep(1)}
-                                className={`status1 step1 ${activeStep === 1 ? 'active' : ''}`}>
-                                <span className="step__text">Added</span>
-                            </div>
-                            <div
-                                onClick={() => setActiveStep(2)}
-
-                                className={`status step2 ${activeStep === 2 ? 'active' : ''}`}>
-                                <span className="step__text">In-check</span>
-                            </div>
-                            <div
-                                onClick={() => setActiveStep(3)}
-                                className={`status step3 ${activeStep === 3 ? 'active' : ''}`}>
-                                <span className="step__text">approved</span>
-                            </div>
-                            <div
-                                onClick={() => setActiveStep(4)}
-                                className={`status step4 ${activeStep === 4 ? 'active' : ''}`}>
-                                <span className="step__text">Active</span>
-                            </div>
-                            <div
-                                onClick={() => setActiveStep(5)}
-                                className={`status step5 ${activeStep === 5 ? 'active' : ''}`}>
-                                <span className="step__text">Inactive</span>
-                            </div>
+                            {steps.map((step, index) => {
+                                return (
+                                    <div
+                                        key={index}
+                                        onClick={() => setActiveStep(index)}
+                                        className={`${index === 0 ? 'step1' : 'step'} step${index+1} ${activeStep === index ? 'active' : ''}`}>
+                                        <span className="step__text">{step.text}</span>
+                                    </div>
+                                )
+                            })}
                         </div>
-
                     </div>
                 )
             }
